2c066e8ee63764cb13f59cf6aebca019814eea35
akm762xm4 <shahfarhan4451@gmail.com>
2025-05-28 14:15:42 +0530

    minor fix in serverApi

--- src/app/server-api.ts
+++ src/app/server-api.ts
@@ -1,9 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = "https://notes-mern-server.vercel.app";
+
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://notes-mern-server.vercel.app",
+    baseUrl: baseUrl,
   }),
   endpoints: () => ({}),
   tagTypes: ["note", "user"],
-})
+});
